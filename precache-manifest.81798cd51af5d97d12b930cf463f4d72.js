6ab4578b11ca1cb6282828944d8b861e6c666bf4
dazedbear <angus912584@gmail.com>
2019-02-05 20:38:17 +0800

    Updates

---
+++ precache-manifest.81798cd51af5d97d12b930cf463f4d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f3393324dfbb61aef8b",
+    "url": "/apim/static/js/runtime~main.2f339332.js"
+  },
+  {
+    "revision": "fcd0f07260ec7072c7ce",
+    "url": "/apim/static/js/main.fcd0f072.chunk.js"
+  },
+  {
+    "revision": "bf22c3c350ea8f48f3b7",
+    "url": "/apim/static/js/1.bf22c3c3.chunk.js"
+  },
+  {
+    "revision": "fcd0f07260ec7072c7ce",
+    "url": "/apim/static/css/main.bcead1e1.chunk.css"
+  },
+  {
+    "revision": "bf22c3c350ea8f48f3b7",
+    "url": "/apim/static/css/1.47b761e2.chunk.css"
+  },
+  {
+    "revision": "cbf86e679553c8b998401bccf9c9799f",
+    "url": "/apim/index.html"
+  }
+];
