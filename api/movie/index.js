395e916f3f2419f4f35843a5d5a8518b0e9743bf
angus.chen <angus.chen@104.com.tw>
2019-02-07 17:28:23 +0800

    fix(movie): 修正 lambda bug
    1. 修正 offset、limit 數字與文字運算問題
    2. response 改用 lambda proxy response

--- api/movie/index.js
+++ api/movie/index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 const dayjs = require('dayjs');
 const redis = require('redis');
 const { promisify } = require('util');
-const { resTemplate, warnTemplate, errorTemplate, validationError, pagingModel } = require('/opt/nodejs/util');
+const { resTemplate, warnTemplate, errorTemplate, validationError, pagingModel, lambdaProxyResponse } = require('/opt/nodejs/util');
 
 const openDataAPI = 'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=8';
 
@@ -21,9 +21,9 @@ const sortMovies = (movies = [], sortType, offset = 0, limit = 10) =>
     const validSortType = ['latest', 'hot', 'free'];
     if (!validSortType.includes(sortType))
       return reject(validationError(`Invalid sortType: ${sortType}`, 211));
-    if (!Number.isInteger(offset))
+    if (isNaN(parseInt(offset)))
       return reject(validationError(`Invalid offset: ${offset}`, 212));
-    if (!Number.isInteger(limit))
+    if (isNaN(parseInt(limit)))
       return reject(validationError(`Invalid limit: ${limit}`, 213));
     if (!Array.isArray(movies))
       return reject(validationError(`Invalid movies: ${movies}`, 214));
@@ -32,14 +32,14 @@ const sortMovies = (movies = [], sortType, offset = 0, limit = 10) =>
       case 'latest': {
         const result = movies
           .sort((prev, next) => dayjs(prev.startDate).millisecond() > dayjs(next.startDate).millisecond())
-          .slice(offset, offset + limit);
+          .slice(offset, parseInt(offset) + parseInt(limit));
         return resolve(result)
       }
 
       case 'hot': {
         const result = movies
           .sort((prev, next) => prev.hitRate > next.hitRate)
-          .slice(offset, offset + limit);
+          .slice(offset, parseInt(offset) + parseInt(limit));
         return resolve(result)
       }
 
@@ -47,7 +47,7 @@ const sortMovies = (movies = [], sortType, offset = 0, limit = 10) =>
         const result = movies
           .filter(event => event.onSales === 'N')
           .sort((prev, next) => dayjs(prev.startDate).millisecond() > dayjs(next.startDate).millisecond())
-          .slice(offset, offset + limit);
+          .slice(offset, parseInt(offset) + parseInt(limit));
         return resolve(result);
       }
 
@@ -57,6 +57,10 @@ const sortMovies = (movies = [], sortType, offset = 0, limit = 10) =>
     }
   })
 
+/**
+ * 啟用 lambda 代理整合的輸入 (event)
+ * https://docs.aws.amazon.com/zh_tw/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ */
 exports.handler = async (event, context) => {
   const { REDIS_USER, REDIS_PASSWORD, REDIS_SERVER_HOST, REDIS_SERVER_PORT } = process.env;
 
@@ -99,26 +103,29 @@ exports.handler = async (event, context) => {
     .then(rawData => {
       const movies = JSON.parse(rawData);
       // 帶入 API Gateway 的參數得到電影列表
-      const { limit, offset } = event.queryStringParameters
-      const { sortType } = event.pathParameters
+      const { limit, offset } = (event.queryStringParameters || {})
+      const { sortType } = (event.pathParameters || {})
       return Promise.all([
         movies,
-        sortMovies(movies, sortType, Number(offset), Number(limit))
+        sortMovies(movies, sortType, offset, limit)
       ])
     })
     .then(([rawList, movieList]) => {
-      const { limit, offset } = event.queryStringParameters
-      return {
+      const { limit, offset } = (event.queryStringParameters || {})
+      const nextOffset = parseInt(offset || 0) + parseInt(limit || 10);
+      if (isNaN(nextOffset))
+        return Promise.reject(validationError(`Invalid limit or offset: ${limit}, ${offset}`, 215))
+      return lambdaProxyResponse({
         statusCode: 200,
-        body: resTemplate(pagingModel(rawList, movieList, Number(offset) + Number(limit)))
-      };
+        body: resTemplate(pagingModel(rawList, movieList, nextOffset))
+      });
     })
     .catch(e => {
       console.error(e.message)
-      return {
+      return lambdaProxyResponse({
         statusCode: e.isUserError ? 400 : 500,
         body: e.isUserError ? warnTemplate(e.message, e.code) : errorTemplate(e.message, e)
-      }
+      })
     })
   return result;
 }
