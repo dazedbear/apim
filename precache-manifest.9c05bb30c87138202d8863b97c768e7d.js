a2b062eb35bf45eef2d81acfcde9f9f809db3749
angus.chen <angus.chen01@verizonmedia.com>
2019-06-18 16:17:12 +0800

    Updates

---
+++ precache-manifest.9c05bb30c87138202d8863b97c768e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f3393324dfbb61aef8b",
+    "url": "/apim/static/js/runtime~main.2f339332.js"
+  },
+  {
+    "revision": "b57ae77bc21bd068ebd1",
+    "url": "/apim/static/js/main.b57ae77b.chunk.js"
+  },
+  {
+    "revision": "43600c22332cf63748ea",
+    "url": "/apim/static/js/1.43600c22.chunk.js"
+  },
+  {
+    "revision": "b57ae77bc21bd068ebd1",
+    "url": "/apim/static/css/main.fbf1bd2a.chunk.css"
+  },
+  {
+    "revision": "43600c22332cf63748ea",
+    "url": "/apim/static/css/1.5f1ed30c.chunk.css"
+  },
+  {
+    "revision": "324ead489cd2cafc97d8ea0e52673849",
+    "url": "/apim/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/apim/precache-manifest.597ce89222adf6889900556c053e9ecb.js"
+  "/apim/precache-manifest.9c05bb30c87138202d8863b97c768e7d.js"
 );
 
 workbox.clientsClaim();
